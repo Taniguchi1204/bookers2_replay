eca681b15c9db5eddaa139f4f363ca29d15d1b16
Taniguchi1204 <tatsuya1204@gamil.com>
2021-06-19 13:18:16 +0000

    [bookers2]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '22e161af57b1e851c4a3a81a0819b17da6cb392b57d293d6d39f3b278b8ce7919c4690bb81fd66a2d460c7833f8d6678e72eb2447b3e5bc916c29367e1d6f1d0'
